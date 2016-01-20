6d3e3c028564bcae93f943f6fd45603b9821179e
Christopher Brown <io@henrian.com>
2016-01-20 13:55:32 -0600

    Support date: string in DateTime component

--- components/DateTime.tsx
+++ components/DateTime.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import * as moment from 'moment';
+// import * as moment from 'moment'; // doesn't work. weird.
+const moment = require('moment');
 
-const DateTime = ({date, format = 'YYYY-MM-DD h:mm A'}: {date: Date, format?: string}) => {
+const DateTime = ({date, format = 'YYYY-MM-DD h:mm A'}: {date: Date | string, format?: string}) => {
   let text = moment(date).format(format);
-  return <time dateTime={date.toISOString()}>{text}</time>;
+  return <time dateTime={date instanceof Date ? date.toISOString() : date}>{text}</time>;
 };
 DateTime['propTypes'] = {
-  date: React.PropTypes.object.isRequired,
+  date: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.string]).isRequired,
   format: React.PropTypes.string,
 };
 export default DateTime;
