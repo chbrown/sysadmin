500b92a3f25b9b6bf02abc22d3d3ea204777d18f
Christopher Brown <io@henrian.com>
2018-07-11 19:45:31 -0500

    Migrate from http-respond to http-extend (from 2016-04-17)

--- server.ts
+++ server.ts
@@ -2,12 +2,17 @@ import {IncomingMessage, ServerResponse, createServer} from 'http';
 import * as urlio from 'urlio';
 import {logger, Level} from 'loge';
 import {inspect} from 'util';
-import {addBody, addXhr, addUrlObj} from 'http-extend';
-import respond from 'http-respond';
+import {addBody, addXhr, addUrlObj, addCookies} from 'http-extend';
+import {Props, Component, ComponentClass, StatelessComponent, Children, ValidationMap, PropTypes, createElement} from 'react';
+import {renderToString, renderToStaticMarkup} from 'react-dom/server';
 
-import routes, {Route, ResponsePayload} from './routes';
-
+import routes, {Route, ResponsePayload, ReactComponent} from './routes';
 import Root, {ErrorView} from './components/Root';
+
+/**
+A generic version of the standard React.ReactType type (which is also a union).
+*/
+type ReactType<P extends Props<any>> = string | ComponentClass<P> | StatelessComponent<P>;
 
 /**
 Uses `routes` global.
@@ -21,8 +26,25 @@ function addRoute<Req extends {method?: string, pathname: string}>(req: Req) {
   return Object.assign(req, {route, params: route.params});
 }
 
+interface ProviderProps {
+  cookies: {[index: string]: string};
+}
+class Provider extends Component<ProviderProps, {}> {
+  getChildContext() {
+    return {cookies: this.props.cookies};
+  }
+  render() {
+    return Children.only(this.props.children) as any;
+  }
+  static childContextTypes: ValidationMap<any> = {
+    cookies: PropTypes.object.isRequired,
+  }
+}
+
+const _layoutPlaceholder = '#yield#';
+
 function httpHandler(req: IncomingMessage, res: ServerResponse): void {
-  Promise.resolve(req).then(addBody).then(addXhr).then(addUrlObj).then(addRoute)
+  Promise.resolve(req).then(addBody).then(addXhr).then(addUrlObj).then(addCookies).then(addRoute)
   .then(req => {
     // logger.info(`handling params=${inspect(req.params)} query=${inspect(req.query)}`);
     return Promise.resolve(req.route.handler(req))
@@ -37,11 +59,33 @@ function httpHandler(req: IncomingMessage, res: ServerResponse): void {
   .then(({req, payload}) => {
     // logger.info(`rendering payload=${inspect(payload)}`);
     // can I factor these hacks into http-respond somehow?
-    payload = Object.assign(payload, {xhr: req.xhr})
-    if (payload.Component) {
-      payload = Object.assign(payload, {LayoutComponent: Root})
+    // export function send<P>(callback: () => void) {
+    res.statusCode = payload.statusCode || 200;
+    if (payload.headers) {
+      payload.headers.forEach(([name, value]) => res.setHeader(name, value));
     }
-    respond(res, payload);
+
+    if (payload.stream) {
+      payload.stream.pipe(res);
+    }
+    else if (!req.xhr && payload.Component) {
+      res.setHeader('Content-Type', 'text/html');
+
+      const element = createElement(payload.Component, payload.props);
+      const providerElement = createElement(Provider, {cookies: req.cookies}, element);
+      const providerElementHtml = renderToString(providerElement);
+      const layout = createElement(Root, payload.props, _layoutPlaceholder);
+      const layoutHtml = renderToStaticMarkup(layout);
+      const html = '<!DOCTYPE html>' + layoutHtml.replace(_layoutPlaceholder, providerElementHtml);
+      res.end(html);
+    }
+    else if (payload.props) {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(payload.props));
+    }
+    else {
+      res.end();
+    }
   })
   .catch(error => {
     // oops, complete fail, handle any/all errors that occurred in the rendering step here.
