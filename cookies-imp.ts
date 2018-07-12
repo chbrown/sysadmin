57756fcc1746b16c29a3c16ec8a84902427f7494
Christopher Brown <io@henrian.com>
2018-07-11 19:41:56 -0500

    Add server+client cookies logic/React Provider

---
+++ cookies-imp.ts
@@ -0,0 +1,5 @@
+import {CookieOptions} from './cookies';
+
+export function setCookie(name: string, value: string, options: CookieOptions = {}): void {
+  console.error('cannot currently set cookies on the server-side');
+}
---
+++ cookies.ts
@@ -0,0 +1,41 @@
+import {PropTypes, Component, ComponentClass, createElement} from 'react';
+const hoistStatics = require('hoist-non-react-statics');
+
+export interface CookieObject {
+  [index: string]: string;
+}
+
+export interface CookieOptions {
+  expires?: Date | string;
+  path?: string;
+  domain?: string;
+  secure?: boolean;
+}
+
+export const CookiesPropTypes = PropTypes.object.isRequired;
+
+export interface ConnectedCookiesProps {
+  cookies?: CookieObject;
+}
+
+export interface ComponentDecorator<P> {
+  (WrappedComponent: ComponentClass<P>): ComponentClass<P>;
+}
+
+export function connectCookies<P>(): ComponentDecorator<P> {
+  return function wrapWithConnect(WrappedComponent: ComponentClass<P>): ComponentClass<P> {
+    class ConnectCookies extends Component<P, {}> {
+      cookies: CookieObject;
+      constructor(props: P, context: any) {
+        super(props, context);
+        this.cookies = context.cookies;
+      }
+      render() {
+        const mergedProps = Object.assign({}, this.props, {cookies: this.cookies});
+        return createElement(WrappedComponent, mergedProps);
+      }
+      static contextTypes = {cookies: CookiesPropTypes};
+    }
+    return hoistStatics(ConnectCookies, WrappedComponent) as ComponentClass<P>;
+  }
+}
