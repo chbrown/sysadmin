380a4b012b86c14c40eb86d7d9091839b9ba3799
Christopher Brown <io@henrian.com>
2018-09-08 15:56:08 -0500

    Use toLocaleString in DateTime component instead of moment.format

--- components/DateTime.tsx
+++ components/DateTime.tsx
@@ -1,10 +1,47 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import * as moment from 'moment';
 
-const DateTime = ({date, format = 'YYYY-MM-DD h:mm A'}: {date: Date | string, format?: string}) => {
-  let text = moment(date).format(format);
-  return <time dateTime={date instanceof Date ? date.toISOString() : date}>{text}</time>;
+const full: Intl.DateTimeFormatOptions = {
+  weekday:      'long',
+  year:         'numeric',
+  month:        'long',
+  day:          'numeric',
+  hour:         'numeric',
+  minute:       'numeric',
+  second:       'numeric',
+  timeZoneName: 'short',
+}
+const long: Intl.DateTimeFormatOptions = {
+  year:         'numeric',
+  month:        'long',
+  day:          'numeric',
+  hour:         'numeric',
+  minute:       'numeric',
+  second:       'numeric',
+  timeZoneName: 'short',
+}
+const medium: Intl.DateTimeFormatOptions = {
+  year:   'numeric',
+  month:  'short',
+  day:    'numeric',
+  hour:   'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+}
+const short: Intl.DateTimeFormatOptions = {
+  year:   '2-digit',
+  month:  'numeric',
+  day:    'numeric',
+  hour:   'numeric',
+  minute: 'numeric',
+}
+
+const formats = {full, long, medium, short}
+
+const DateTime = ({date, format = 'medium'}: {date: Date | string, format?: string}) => {
+  const dateObject = new Date(date);
+  const localeString = dateObject.toLocaleString('en-US', formats[format]);
+  return <time dateTime={dateObject.toISOString()}>{localeString}</time>;
 };
 DateTime['propTypes'] = {
   date: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
