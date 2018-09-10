d64fa0c32b106e7782dbab577ba1573cabf9d563
Christopher Brown <io@henrian.com>
2018-09-09 21:15:40 -0500

    Move CLI functionality into 'server' module

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
 import {IncomingMessage, ServerResponse, createServer} from 'http'
+import * as optimist from 'optimist'
 import * as urlio from 'urlio'
 import {logger, Level} from 'loge'
 import {inspect} from 'util'
@@ -111,4 +113,42 @@ export function start(port: number = defaultPort, hostname: string = defaultHost
     console.log(`server listening on http://${addressString}`)
   })
   server.listen(port, hostname)
+}
+
+export function main() {
+  const argvparser = optimist
+  .usage('Usage: sysadmin')
+  .describe({
+    help: 'print this help message',
+    verbose: 'print extra output',
+    version: 'print version',
+    port: 'port to listen on',
+    hostname: 'hostname to listen on',
+  })
+  .alias({
+    h: 'help',
+    v: 'verbose',
+  })
+  .default({
+    port: defaultPort,
+    hostname: defaultHostname,
+  })
+  .boolean(['help', 'verbose', 'version'])
+
+  const argv = argvparser.argv
+  logger.level = argv.verbose ? Level.debug : Level.info
+
+  if (argv.help) {
+    argvparser.showHelp()
+  }
+  else if (argv.version) {
+    console.log(require('./package').version)
+  }
+  else {
+    start(parseInt(argv.port, 10), argv.hostname)
+  }
+}
+
+if (require.main === module) {
+  main()
 }
